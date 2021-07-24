e42a0b1f8e8cdbf5c7791e1e9f658982a2551d5d
Bram Esendam <Bram.esendam@gmail.com>
2021-07-24 14:55:16 +0200

    Added extra config with database and batching options

--- src/Serilog.Sinks.Mongodb.TimeSeries/Extensions/LoggerSinkConfigurationExtensions.cs
+++ src/Serilog.Sinks.Mongodb.TimeSeries/Extensions/LoggerSinkConfigurationExtensions.cs
@@ -28,6 +28,23 @@ namespace Serilog.Sinks.Mongodb.TimeSeries.Extensions
             var batchingMongoDbSink = new PeriodicBatchingSink(new MongoDbTimeSink(sinkConfig), batchingOptions);
             return loggerConfiguration.Sink(batchingMongoDbSink);
         }
+        
+        /// <summary>
+        ///     Registers the MongoDb time series sink to the logger.
+        /// </summary>
+        /// <param name="loggerConfiguration">The <see cref="LoggerSinkConfiguration" />.</param>
+        /// <param name="database">The database of where the logs will be stored.</param>
+        /// <param name="batchingOptions">
+        ///     The <see cref="PeriodicBatchingSinkOptions" /> containing the settings that will be used
+        ///     to batch the logs.
+        /// </param>
+        /// <returns>
+        ///     The new <see cref="LoggerSinkConfiguration" /> with new sink.
+        /// </returns>
+        public static LoggerConfiguration MongoDbTimeSeriesSink(this LoggerSinkConfiguration loggerConfiguration, IMongoDatabase database, PeriodicBatchingSinkOptions batchingOptions)
+        {
+            return MongoDbTimeSeriesSink(loggerConfiguration, new MongoDbTimeSeriesSinkConfig(database), batchingOptions);
+        }
 
         /// <summary>
         ///     Registers the MongoDb time series sink to the logger.
@@ -45,7 +62,7 @@ namespace Serilog.Sinks.Mongodb.TimeSeries.Extensions
                 Period = TimeSpan.FromSeconds(10),
                 EagerlyEmitFirstEvent = true
             };
-            return MongoDbTimeSeriesSink(loggerConfiguration, new MongoDbTimeSeriesSinkConfig(database), defaultSinkOptions);
+            return MongoDbTimeSeriesSink(loggerConfiguration, database, defaultSinkOptions);
         }
     }
 }
