db9def59af78e815ea6c0b3a6dc319f997296e86
Bram Esendam <Bram.esendam@gmail.com>
2021-07-26 00:13:53 +0200

    Added ToDocuments to correct extensions class

---
+++ src/Serilog.Sinks.Mongodb.TimeSeries/Extensions/LogEventExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Serilog.Events;
+using Serilog.Sinks.Mongodb.TimeSeries.Models;
+
+namespace Serilog.Sinks.Mongodb.TimeSeries.Extensions
+{
+    /// <summary>
+    ///     Contains all extensions methods for <see cref="LogEvent" />.
+    /// </summary>
+    public static class LogEventExtensions
+    {
+        /// <summary>
+        ///     Converts a <see cref="IEnumerable{T}" /> of <see cref="LogEvent" />s to <see cref="IEnumerable{T}" /> of
+        ///     <see cref="LogDocument" />s.
+        /// </summary>
+        /// <param name="logEvents">The log events that will be converted.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>
+        ///     The converted <see cref="IEnumerable{T}" /> of <see cref="LogDocument" />s.
+        /// </returns>
+        internal static IEnumerable<LogDocument> ToDocuments(this IEnumerable<LogEvent> logEvents, IFormatProvider? formatProvider = null)
+        {
+            var logs = new List<LogDocument>();
+
+            foreach (var logEvent in logEvents)
+            {
+                var message = logEvent.RenderMessage(formatProvider);
+                var properties = new Dictionary<string, string>();
+
+                foreach (var (key, value) in logEvent.Properties) properties.Add(key.ToSaveAbleString(), value.ToString());
+
+                logs.Add(new LogDocument
+                {
+                    Exception = logEvent.Exception,
+                    Severity = logEvent.Level.ToSeverityString(),
+                    Properties = properties,
+                    Timestamp = logEvent.Timestamp.UtcDateTime,
+                    Message = message
+                });
+            }
+
+            return logs;
+        }
+    }
+}
