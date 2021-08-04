032f6f5b72202a272f5beeaeaa48ecd5305afe15
Bram <Bram.esendam@gmail.com>
2021-08-05 00:28:19 +0200

    Fix: Made Config properties init only

--- src/Serilog.Sinks.Mongodb.TimeSeries/Configurations/MongoDbTimeSeriesSinkConfig.cs
+++ src/Serilog.Sinks.Mongodb.TimeSeries/Configurations/MongoDbTimeSeriesSinkConfig.cs
@@ -23,61 +23,61 @@ namespace Serilog.Sinks.Mongodb.TimeSeries.Configurations
         /// <summary>
         ///     The collection name of where the logs will be stored. The default is "logs".
         /// </summary>
-        public string CollectionName { get;  } = "logs";
+        public string CollectionName { get; init; } = "logs";
 
         /// <summary>
         ///     The maximum number of log events to include in a single batch. The default is 1000.
         /// </summary>
-        public int BatchSizeLimit { get;  } = 1000;
+        public int BatchSizeLimit { get; init; } = 1000;
 
         /// <summary>
         ///     Maximum number of events to hold in the sink's internal queue, or null for an unbounded queue. The default is 10000.
         /// </summary>
-        public int QueueLimit { get;  } = 10000;
+        public int QueueLimit { get; init; } = 10000;
 
         /// <summary>
         ///     Eagerly emit a batch containing the first received event, regardless of the target batch size or batching time.
         ///     This helps with perceived "liveness" when running/debugging applications interactively. The default is true.
         /// </summary>
-        public bool EagerlyEmitFirstEvent { get;  } = true;
+        public bool EagerlyEmitFirstEvent { get; init; } = true;
 
         /// <summary>
         ///     The time to wait between checking for event batches. The default is 5 seconds.
         /// </summary>
-        public TimeSpan SyncingPeriod { get;  } = TimeSpan.FromSeconds(5);
+        public TimeSpan SyncingPeriod { get; init; } = TimeSpan.FromSeconds(5);
 
         /// <summary>
         ///     The coarse granularity of time-series data. The default is Seconds.
         /// </summary>
-        public TimeSeriesGranularity TimeSeriesGranularity { get;  } = TimeSeriesGranularity.Seconds;
+        public TimeSeriesGranularity TimeSeriesGranularity { get; init; } = TimeSeriesGranularity.Seconds;
 
         /// <summary>
         ///     Gets or sets the maximum size of the collection in bytes.
         ///     Logs will get deleted when this number has been hit.
         /// </summary>
-        public long? MaxCollectionSize { get;  }
+        public long? MaxCollectionSize { get; init; }
 
         /// <summary>
         ///     Gets or sets the maximum number of logs that will be stored.
         ///     Logs will get deleted when this number has been hit.
         /// </summary>
-        public long? MaxLogsAmount { get;  }
+        public long? MaxLogsAmount { get; init; }
 
         /// <summary>
         ///     Gets or sets a timespan indicating how long documents in a time series collection should be retained.
         /// </summary>
-        public TimeSpan? LogsExpireAfter { get;  }
+        public TimeSpan? LogsExpireAfter { get; init; }
 
         /// <summary>
         ///     The <see cref="MongoDB.Driver.CreateCollectionOptions" /> that will be used to create a new collection
         ///     when no collection with the name of <see cref="CollectionName" /> exists.
         /// </summary>
-        internal CreateCollectionOptions CreateCollectionOptions { get;  }
+        internal CreateCollectionOptions CreateCollectionOptions { get; init; }
 
         /// <summary>
         ///     The <see cref="IMongoDatabase" /> of where the collection will be stored..
         /// </summary>
-        internal IMongoDatabase Database { get;  }
+        internal IMongoDatabase Database { get; init; }
 
         /// <summary>
         ///     Get a default implementation of <see cref="MongoDB.Driver.CreateCollectionOptions" />.
